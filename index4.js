4d1a8eef0859f84a419580f8a4b60c61fb06b070
meghanadhanvate <meghnadhanawate@gmail.com>
2022-06-08 22:57:28 +0530

    km

---
+++ index4.js
@@ -0,0 +1,8 @@
+function validationForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if(x == " ")
+    {
+        alert("Name must be filled out");
+        return false;
+    }
+}
